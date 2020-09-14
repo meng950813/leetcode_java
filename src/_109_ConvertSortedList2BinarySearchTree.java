4df60ec2e1f093a5fca540f9a7d0852d44680821
i2lmeng <1752397568@qq.com>
2020-09-14 11:33:17 +0800

    modify: 移除文件内 TreeNode 类

--- src/_109_ConvertSortedList2BinarySearchTree.java
+++ src/_109_ConvertSortedList2BinarySearchTree.java
@@ -70,22 +70,3 @@ class ListNode2 {
         this.next = next;
     }
 }
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {}
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
